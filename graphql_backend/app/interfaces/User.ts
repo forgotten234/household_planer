2c14467045289c7bace4eb14b486fd783b3d5630
Tim S <tim.seegert@student.htw-berlin.de>
2023-05-02 12:42:21 +0300

    login/logout ready. settings page on the way

--- graphql_backend/app/interfaces/User.ts
+++ graphql_backend/app/interfaces/User.ts
@@ -1,11 +1,16 @@
 import {Document} from 'mongoose';
 interface User extends Document {
+  first_name?: string;
+  second_name?: string;
   user_name: string;
   email: string;
   password: string;
+  role?: string;
 }
 
 interface UserTest {
+  first_name?: string;
+  second_name?: string;
   id?: string;
   user_name?: string; // returned from graphql is snake_case
   userName?: string; // graphql variables are camelCase
